cd8bfe2360d75e31f1cb35103b01d07efd259430
aledec <aledec@aledec.com.ar>
2015-07-21 19:01:45 -0300

    deleted:    ../encriptacion/encripted.sh 	deleted:    dbverify/dbverify_semanal.config.txt 	deleted:    dbverify/dbverify_semanal.sh 	deleted:    dbverify/dbverify_semanal.sh.log 	deleted:    dbverify/dbverify_semanal.sh~ 	deleted:    dbverify/sendEmail.pl

--- encriptacion/encripted.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cryptsetup luksOpen /dev/VolGroup01/LogVolEncrypted crypt_VolGroup01_LogVolEncrypted
-udevadm settle
-dmsetup table crypt_VolGroup01_LogVolEncrypted 
-mount /dev/mapper/crypt_VolGroup01_LogVolEncrypted /encrypted/
