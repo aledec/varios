45c027fbb32433c42c230f6bc0a5b4ba50ceacc2
aledec <aledec@aledec.com.ar>
2015-07-21 19:13:31 -0300

    renamed:    update_printers/README -> etc_hosts_updater/README 	renamed:    update_printers/contingencia/actualizar_hosts_contingencia_impresoras_sap.sh -> etc_hosts_updater/contingencia/actualizar_hosts_contingencia_impresoras_sap.sh 	renamed:    update_printers/produccion/actualizar_hosts_produccion_sap.sh -> etc_hosts_updater/produccion/actualizar_hosts_produccion_sap.sh 	renamed:    update_printers/produccion/rsync_update_hosts.sh -> etc_hosts_updater/produccion/rsync_update_hosts.sh 	renamed:    update_printers/pruebas/hosts -> etc_hosts_updater/pruebas/hosts 	deleted:    monitoreo-backup-servidores/.backups_new.sh.tmp.sinbackupsemanal.kate-swp 	deleted:    monitoreo-backup-servidores/backups_new.sh 	deleted:    monitoreo-backup-servidores/listado_equipos.sh 	deleted:    monitoreo-backup-servidores/listado_servidores_backups.txt 	renamed:    monitoreo/monitoreo.sh -> server_monitoring_icmp_port/monitoreo.sh 	modified:   README.md

--- monitoreo-backup-servidores/listado_equipos.sh
+++
@@ -1,1 +0,0 @@
-cat backups.txt|grep -i sua|sed 's/----------------------------------->//'|sed 's/-------------------------------------//'|sed 's/\#//'|awk '{print $1}'|sort|awk -F@ '{print $1}'|uniq > equipos.txt
